a1bd440b31bb03432070613c975113cd197da39c
Rounaq <rounaqwl66@gmail.com>
2016-04-18 12:36:23 +0530

    Maintenance #2

--- MobileApp/app/common/index.js
+++ MobileApp/app/common/index.js
@@ -7,16 +7,53 @@ var SERVER_ADDRESS = "192.168.1.14:5000";
 var acTemp = 0;
 
 $(document).ready(function(){
+    if(localStorage.getItem("SERVER_ADDRESS") == "undefined")
+    localStorage.setItem("SERVER_ADDRESS",SERVER_ADDRESS);
+    else SERVER_ADDRESS = localStorage.getItem("SERVER_ADDRESS");
     $("#temperature_val").html(acTemp+" °C");
     $("#btn-ac-dec").click(function(){
         acTemp -= 1
         showMax();
     });
     $("#btn-ac-inc").click(function(){
-        acTemp += 1    
+        acTemp += 1
         showMax();
     });
+    $("#btn-ac-sync").click(function(){
+
+    });
+    syncTemp();
 });
+
+function syncHardware()
+{
+    $.get("http://"+SERVER_ADDRESS+'/getDeviceStatuses',function(data)
+    {
+        for(i=0;i<15;i++)
+        {
+            if(data[str(i)]) $("#"+str(i)).prop("checked",true);
+            else $("#"+str(i)).prop("checked",false);
+        }
+    },"json");
+    setTimeout(syncHardware,10000);
+}
+
+function syncTemp()
+{
+    $.get("http://"+SERVER_ADDRESS+'/getTemperature',function(data)
+    {
+        acTemp = parseFloat(data);
+        showMax();    
+    });
+    $.get("http://"+SERVER_ADDRESS+'/getOptimalTemperature',function(data)
+    {
+        $("#optimal_temperature_val").innerHTML("Optimal Temperature: "+data);
+        showMax();
+    });
+
+    setTimeout(syncTemp,5000);
+}
+
 function showMax()
 {
         //alert(SERVER_ADDRESS+'/setTemperature?val='+acTemp.toString());
